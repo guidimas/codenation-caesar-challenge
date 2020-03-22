dd5fd6885dfc2642907eb405b3ba1dfc5d3b8157
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-03-22 20:32:17 -0300

    Caesar functions extracted to a new module.

---
+++ caesar.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const sha1 = require('sha1');
+const fs = require('fs');
+require('dotenv/config');
+
+const api = axios.create({
+  baseURL: 'https://api.codenation.dev/v1/challenge/dev-ps'
+});
+
+const getChallengeData = async () => {
+  try {
+    return await api.get(`/generate-data?token=${process.env.CODENATION_TOKEN}`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const mod = (number, mod) => {
+  return ((number % mod) + mod) % mod;
+}
+
+const decryptCaesar = (shift, encrypted, alphabet) => {
+  return encrypted.split('')
+                  .map(letter => (alphabet.indexOf(letter) > -1) ? alphabet[mod(alphabet.indexOf(letter) - shift, alphabet.length)] : letter)
+                  .join('');
+}
+
+const saveToDisk = answer => {
+  fs.writeFile('answer.json', JSON.stringify(answer), error => {
+    if (error) return console.error(error);
+  });
+}
+
+module.exports = {
+  generateAnswer: async () => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const { data: challengeData } = await getChallengeData();
+
+    if (challengeData) {
+      let answer = {...challengeData};
+      answer.decifrado = decryptCaesar(answer.numero_casas, answer.cifrado, alphabet);
+      answer.resumo_criptografico = sha1(answer.decifrado);
+      saveToDisk(answer);
+      console.log('Answer file generated.');
+    } else {
+      console.error("Whoops! Something went wrong.");
+    }
+  }
+};
