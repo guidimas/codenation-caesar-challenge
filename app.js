4b733372a17c661dd16a8ab83ad6c98c76aeea89
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-03-22 21:15:16 -0300

    Answer sending page created (with pug, added to dependencies).

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
 const caesar = require('./caesar');
+const port = 3000;
 
-caesar.generateAnswer();
+require('dotenv/config');
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  caesar.generateAnswer();
+  res.render('index', { token: process.env.CODENATION_TOKEN });
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
